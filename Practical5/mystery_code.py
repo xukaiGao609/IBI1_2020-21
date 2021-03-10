b11790e4c67cfcb86a136df949800a9bcbbb9007
xukaiGao609 <79949021+xukaiGao609@users.noreply.github.com>
2021-03-10 19:49:19 +0800

    Update mystery_code.py

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: the code will random produce a number which is not bigger than 50
+# Answer: the code will randomly produce a number which is not bigger than 50
 
 # Import libraries
 # randint allows drawing a random number,
