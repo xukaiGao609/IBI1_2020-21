b05eae92b57ff66c3e7913571b1dab0e79c64e0a
xukaiGao609 <79949021+xukaiGao609@users.noreply.github.com>
2021-03-11 16:13:28 +0800

    Delete Practical5 directory

--- Practical5/collatz.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b = 1
-print(a)
-print(b) # to make sure that the outcome contains the first two values of Fibonacci
-for i in range(1,12):# make sure to get 13 values
-    c = a + b # get the value, prepare for the next round
-    a = b
-    b = c
-    print(c) # show the target value we get 
