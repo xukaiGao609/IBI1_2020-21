dc4df5350582f62624bc02741474891e755b9f4e
xukaiGao609 <79949021+xukaiGao609@users.noreply.github.com>
2021-03-17 10:03:28 +0800

    Add files via upload

--- Practical6/coronavirus_rates.py
+++ Practical6/coronavirus_rates.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 frequency = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':4360823, 'UK':4234924}
+print(frequency)
 labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
 sizes = [frequency['USA'],frequency['India'],frequency['Brazil'],frequency['Russia'],frequency['UK']]#build a connection between sizes and dictionary
 explode = (0,0,0,0,0)
