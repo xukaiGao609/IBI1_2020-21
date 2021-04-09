e9518ee2ffc62ca06e9fd4a728a53dbd9beaf33d
xukaiGao609 <79949021+xukaiGao609@users.noreply.github.com>
2021-04-09 16:36:43 +0800

    Update coronavirus_rates.py

--- Practical6/coronavirus_rates.py
+++ Practical6/coronavirus_rates.py
@@ -5,5 +5,6 @@ labels = 'USA', 'India', 'Brazil', 'Russia', 'UK'
 sizes = [frequency['USA'],frequency['India'],frequency['Brazil'],frequency['Russia'],frequency['UK']]#build a connection between sizes and dictionary
 explode = (0,0,0,0,0)
 plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',startangle=90)
-plt.axis('equal')#make sure it is a standard cycle
+plt.axis('equal')#make sure it is a standard cycle instead of a ellipse for example
+plt.title('coronavirus infection rates across countries')
 plt.show()
