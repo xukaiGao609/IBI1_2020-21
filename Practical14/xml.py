9979db2070188041cc11180a6b158756b17a535c
xukaiGao609 <79949021+xukaiGao609@users.noreply.github.com>
2021-05-18 11:28:07 +0800

    Add files via upload

--- Practical14/xml.py
+++ Practical14/xml.py
@@ -3,41 +3,44 @@ import xml.dom.minidom
 import matplotlib.pyplot as plt
 import numpy as np
 
-def parse_xml(path):
+# parsing xml file
+def parse(path):
     DOMTree = xml.dom.minidom.parse(path)
     collection = DOMTree.documentElement
     terms = collection.getElementsByTagName("term")
     return terms
 
+#  getting the parent nodes (molecule_name in <defstr> of the parent nodes)
 def get_primary_matches(terms, molecule_name):
-    match_list = []
+    list = []
     for term in terms:
         defstr_text = term.getElementsByTagName("defstr")[0].childNodes[0]
         data = defstr_text.data
         if molecule_name in data:
-            match_list.append(term)
-    return match_list
+            list.append(term)
+    return list
 
+# define classes Vertex and Graph to structurize terms
 class Vertex:
     def __init__(self, key):
         self.id = key
-        self.connectedTo = {}
+        self.connected = {}
 
     def addNeighbor(self, nbr, weight=0):
-        self.connectedTo[nbr] = weight
+        self.connected[nbr] = weight
 
     def __str__(self):
         return str(self.id) + ' connectedTo: ' \
-               + str([x.id for x in self.connectedTo])
+               + str([x.id for x in self.connected])
 
     def getConnection(self):
-        return self.connectedTo.keys()
+        return self.connected.keys()
 
-    def getId(self):
+    def Id(self):
         return self.id
 
     def getWeight(self, nbr):
-        return self.connectedTo[nbr]
+        return self.connected[nbr]
 
 class Graph:
     def __init__(self):
@@ -72,7 +75,8 @@ class Graph:
     def __iter__(self):
         return iter(self.vertList.values())
 
-def buildTree(terms):
+# build tree of terms
+def Tree(terms):
     tree = Graph()
     for term in terms:
         id = term.getElementsByTagName("id")[0].childNodes[0].data
@@ -81,6 +85,7 @@ def buildTree(terms):
             tree.addEdge(id, child)
     return tree
 
+# assistant recursive function to find all childNodes as vertices
 def getAllChildren(tree, vertices):
     allChildren = []
     for vertex in vertices:
@@ -93,33 +98,38 @@ def getAllChildren(tree, vertices):
                 allChildren += getAllChildren(tree, children)
     return allChildren
 
-def count_childNodes_macromolecules_xml(tree, molecule_name):
-    match_list = get_primary_matches(terms, molecule_name)
-    vertices_list = [tree.getVertex(term.getElementsByTagName("id")[0].childNodes[0].data) for term in match_list]
-    count = len(list(set(getAllChildren(tree, vertices_list)))) - len(match_list)
+# main function outputting the final result
+def count_macromolecules(tree, molecule_name):
+    list = get_primary_matches(terms, molecule_name)
+    vertices_list = [tree.getVertex(term.getElementsByTagName("id")[0].childNodes[0].data) for term in list]
+    count = len(list(set(getAllChildren(tree, vertices_list)))) - len(list)
     return count
 
+# direct absolute path
 path = "C:/Users/11877/github/IBI1_2020-21/Practical14/go_obo.xml"
 
-terms = parse_xml(path)
+# Parse the xml file
+terms = parse(path)
 
-tree = buildTree(terms)
+# build tree
+tree = Tree(terms)
 
-dna = count_childNodes_macromolecules_xml(tree, "DNA")  # 7271
-rna = count_childNodes_macromolecules_xml(tree, "RNA")  # 8927
-protein = count_childNodes_macromolecules_xml(tree, "protein")  # 27771
-carbo = count_childNodes_macromolecules_xml(tree, "carbohydrate")  # 4743
+# Calculate number of each term
+dna = count_macromolecules(tree, "DNA")
+rna = count_macromolecules(tree, "RNA")
+protein = count_macromolecules(tree, "protein")
+carbo = count_macromolecules(tree, "carbohydrate")
 
-print("Number of childNodes of all DNA-associated terms: {}".format(dna))
-print("Number of childNodes of all RNA-associated terms: {}".format(rna))
-print("Number of childNodes of all protein-associated terms: {}".format(protein))
-print("Number of childNodes of all carbohydrate-associated terms: {}".format(carbo))
+print("Number of DNA-associated terms: {}".format(dna))
+print("Number of RNA-associated terms: {}".format(rna))
+print("Number of protein-associated terms: {}".format(protein))
+print("Number of carbohydrate-associated terms: {}".format(carbo))
 
-# Plot the pie chart
+# draw the pie chart
 data = {"DNA": dna, "RNA": rna,
         "Protein": protein, "Carbohydrate": carbo}
 values = np.array([i for i in data.values()])
 labels = np.array([j for j in data.keys()])
 plt.pie(values, labels=labels, shadow=True, autopct='%1.1f%%')
-plt.title("Child Nodes of 4 macromolecules")
+plt.title("proportion of 4 macromolecules")
 plt.show()
